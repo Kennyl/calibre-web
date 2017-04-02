df1d96f211767ec1527bc6959d282f5548035d69
Kennyl <Kennyl@users.noreply.github.com>
2017-04-02 21:14:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
-language: python
-
-python:
-  - "2.7"
-  - "3.4"
-
-install: false
-
-# branches:
-#   only:
-#     - master
-
-script: ./build.sh
+#!/bin/sh
+set -x
+ 
+python -m py_compile cps.py
+python -m py_compile cps/book_formats.py
+python -m py_compile cps/db.py
+python -m py_compile cps/epub.py
+python -m py_compile cps/fb2.py
+python -m py_compile cps/helper.py
+python -m py_compile cps/ub.py
+python -m py_compile cps/uploader.py
+python -m py_compile cps/web.py
+python -m py_compile cps.py 
